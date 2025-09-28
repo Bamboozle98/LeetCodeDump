bf14a1eac84462da6cb69db3790a92703bdea7f3
Brannon McCutcheon <89475103+Bamboozle98@users.noreply.github.com>
2025-09-28 12:26:26 -0400

    Create RotateArray.py

---
+++ Top50Interview/RotateArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n  
+        nums[:] = nums[-k:] + nums[:-k]
+            
+        
+            
+        
+        
